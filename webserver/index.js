61e50c865e9caed15c4f1207f8ad168df49ce28f
DaVinciTachyon <davincitachyon@gmail.com>
2019-12-17 19:17:37 +0000

    public

--- webserver/index.js
+++ webserver/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
+app.use(express.static('public'));
+
+app.get('/endpoint/', (req, res) => {
 	res.send('Hello');
 });
 
